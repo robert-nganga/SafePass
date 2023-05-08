ea09766267ead6688ae7c00853e84db163d79a61
robert-nganga <robertnganga220@gmail.com>
2023-05-08 15:55:18 +0300

    made changes on the text fields

--- app/src/main/java/com/robert/passwordmanager/ui/MainActivity.kt
+++ app/src/main/java/com/robert/passwordmanager/ui/MainActivity.kt
@@ -2,13 +2,12 @@ package com.robert.passwordmanager.ui
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
--- app/src/main/java/com/robert/passwordmanager/ui/fragments/AddAccountFragment.kt
+++ app/src/main/java/com/robert/passwordmanager/ui/fragments/AddAccountFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.robert.passwordmanager.R
 import com.robert.passwordmanager.databinding.FragmentAddAccountBinding
+import androidx.core.widget.addTextChangedListener
+import com.robert.passwordmanager.utils.Validation
+import com.robert.passwordmanager.utils.Validation.validatePassword
 
 class AddAccountFragment: Fragment(R.layout.fragment_add_account) {
 
@@ -24,6 +27,13 @@ class AddAccountFragment: Fragment(R.layout.fragment_add_account) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        passwordFocusListener()
+    }
+
+    private fun passwordFocusListener() {
+        binding.txtPassword.addTextChangedListener {editable->
+            binding.passwordTextField.helperText = validatePassword(editable.toString())
+        }
     }
 
     override fun onDestroyView() {
